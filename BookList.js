a687dba877d9e1a7c640182ad607cc51c2569af5
Brian Bak <65845124+brianjbak@users.noreply.github.com>
2023-05-02 17:09:43 -0700

    Add files via upload

---
+++ BookList.js
@@ -0,0 +1,13 @@
+import styled from "styled-components"
+import Book from "./Book"
+export default function BookList({ books }) {
+    return <Wrapper>
+        { books.map(b => <Book title={b.title} author={b.author} link={b.link} pages={b.pages} />)}
+    </Wrapper>
+}
+
+const Wrapper = styled.div`
+    display: flex;
+    border: 4px solid brown;
+    padding: 5px;
+`
